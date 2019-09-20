387479d7c3dfa22b0090af03d5280b31d7bcf7d9
cmkishores <cmkishores@gmail.com>
2019-09-20 12:51:34 +0530

    Added notes adding deleting and updating forms from user

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import NotesListView,DetailNoteView
+from .views import NotesListView,DetailNoteView,AddNotesView,EditNotesView,DeleteNoteView
 
 urlpatterns= [
 	path('notes/',NotesListView.as_view(),name='notes'),
 	path('notes/<int:pk>/',DetailNoteView.as_view(),name='detailed'),
+	path('notes/new/', AddNotesView.as_view(),name='addnotes'),
+	path('notes/<int:pk>/edit',EditNotesView.as_view(),name='editnotes'),
+	path('notes/<int:pk>/delete/',DeleteNoteView.as_view(),name='deletenotes')
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 from .models import Note
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 
 class NotesListView(ListView):
 	model = Note
@@ -11,5 +13,19 @@ class DetailNoteView(DetailView):
 	model = Note
 	template_name = 'note_detailed.html'
 
+class AddNotesView(CreateView):
+	model = Note
+	template_name = 'addnote.html'
+	fields = ['title','detail']
+
+class EditNotesView(UpdateView):
+	model = Note
+	template_name = 'editnote.html'
+	fields = ['title','detail']
+
+class DeleteNoteView(DeleteView):
+	model = Note
+	template_name = 'deletenote.html'
+	success_url = reverse_lazy('notes')
 
 # Create your views here.
